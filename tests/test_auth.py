a2e5046ea0b73008e7aea5d6be4ba14b5727ef75
mahirpatel04 <patel.mahir.h@gmail.com>
2024-12-21 02:27:22 -0800

    added some auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -33,10 +33,50 @@ async def test_create_user(client):
         "first_name": "Test",
         "birthdate": "1990-01-01",
         "private": False # Adjust based on schema
-}
+        }
 
     response = client.post("/auth/create-user", json=payload)
     assert response.status_code == 200
     data = response.json()
     assert data["username"] == payload["username"]
     assert data["email"] == payload["email"]
+    
+
+@pytest.mark.asyncio
+async def test_duplicate_create_user(client):
+    payload = {
+        "username": "duplicate_create_user",
+        "email": "duplicate_create_user@example.com",
+        "password": "test_password",
+        "first_name": "Test",
+        "birthdate": "1990-01-01",
+        "private": False # Adjust based on schema
+        }
+
+    response = client.post("/auth/create-user", json=payload)
+    assert response.status_code == 200
+    data = response.json()
+    assert data["username"] == payload["username"]
+    assert data["email"] == payload["email"]
+    
+    payload = {
+        "username": "duplicate_create_user",
+        "email": "duplicate_create_user123@example.com",
+        "password": "test_password",
+        "first_name": "Test",
+        "birthdate": "1990-01-01",
+        "private": False # Adjust based on schema
+        }
+
+    second_response = client.post("/auth/create-user", json=payload)
+    assert second_response.status_code == 409
+    
+
+@pytest.mark.asyncio
+async def test_bad_payload(client):
+    payload = {
+        "message": "this is a bad payload for the test"
+        }
+
+    response = client.post("/auth/create-user", json=payload)
+    assert response.status_code == 422
